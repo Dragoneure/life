fcaf340cc3784ca952b20e0e6c59b9631ca6c389
Riwan <coefficriwan@gmail.com>
2024-04-27 10:40:48 +0200

    feat: run script, update Makefile
    - add run script to load ouichefs module, mount a test directory, run the tests and cleanup
    - use provided env KERNELDIR only if defined, else use default one
    - add scripts phony target

---
+++ scripts/run.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# load ouichefs
+insmod /share/ouichefs.ko
+
+# mount a ouichefs test directory
+TESTDIR=~/tests-ouichefs
+if [ ! -d "$TESTDIR" ]; then
+  mkdir $TESTDIR
+  mount /share/test.img $TESTDIR
+fi
+
+# run the user executables
+cd $TESTDIR
+/share/benchmark.o
+cd ~
+
+# cleanup
+umount $TESTDIR
+rm -rf $TESTDIR
+rmmod ouichefs
