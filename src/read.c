2eba925adb3fa313c08bec8307658541e1588994
Harena Rakotondratsima <harenamisa02@gmail.com>
2024-04-30 11:03:08 +0200

    feat: tomanage cat
    Cat function works great with our read, but we do have a problem with
    diff function, it tells that we don't have a \0 compare to an other file
    located on another file system

--- src/read.c
+++ src/read.c
@@ -6,11 +6,15 @@
 #include "linux/types.h"
 #include "ouichefs.h"
 
+/*
+	 * Concurency has been managed in upper layer (ksys_read), no need
+	 * to manage it here, we are already  safe here
+ 	 */
 ssize_t ouichefs_read(struct file *file, char __user *buff, size_t size,
 		      loff_t *pos)
 {
-	pr_info("size to read : %lu\n", size);
-	pr_info("New version new\n");
+	//pr_info("size to read : %lu\n", size);
+	//pr_info("New version new\n");
 	struct buffer_head *bh_index, *bh_data;
 	struct ouichefs_file_index_block *index;
 	/*
@@ -22,7 +26,7 @@ ssize_t ouichefs_read(struct file *file, char __user *buff, size_t size,
 	/*the pos cursor inside a block*/
 	int logical_pos = (*pos) % OUICHEFS_BLOCK_SIZE;
 
-	pr_info("pos : %lld\n", *pos);
+	//pr_info("pos : %lld\n", *pos);
 	struct inode *inode = file->f_inode;
 	struct ouichefs_inode_info *ci = OUICHEFS_INODE(inode);
 	sector_t index_block = ci->index_block;
@@ -31,12 +35,13 @@ ssize_t ouichefs_read(struct file *file, char __user *buff, size_t size,
 	sector_t sc;
 	/*inode -> i_blocks, total number of the file, including the block of index*/
 	int nb_blocks = inode->i_blocks - 1;
-	pr_info("nb_block : %d\n", nb_blocks);
-	pr_info("file size : %lld\n", inode->i_size);
+	//pr_info("nb_block : %d\n", nb_blocks);
+	//pr_info("file size : %lld\n", inode->i_size);
 
 	size_t available_size = 0;
 	int last_bloc_size;
-	int total_read = 1;
+	int total_read = 0;
+	int buff_offset = 1;
 	/* Amount of data to be read inside one block*/
 	int to_read = 0;
 	char *data;
@@ -45,17 +50,17 @@ ssize_t ouichefs_read(struct file *file, char __user *buff, size_t size,
 	if (!bh_index)
 		return -EIO;
 	index = (struct ouichefs_file_index_block *)bh_index->b_data;
-	/*
-	 * Concurency has been managed in upper layer (ksys_read), no need
-	 * to manage it here, we are already  safe here
- 	 */
 
 	last_bloc_size = inode->i_size % OUICHEFS_BLOCK_SIZE;
-	if (last_bloc_size == 0)
+	/* if the last bock of the file is full
+	 * It's possible that the modulo return 0 if the file is
+	 * empty, so last_block_size_should be 0
+	 */
+	if ((last_bloc_size == 0) && (nb_blocks > 1))
 		last_bloc_size = OUICHEFS_BLOCK_SIZE;
-	pr_info("last block size : %d\n", last_bloc_size);
+	//pr_info("last block size : %d\n", last_bloc_size);
 	while ((remaining_read) && (block_logical_number < nb_blocks)) {
-		pr_info("number of round : %d\n", block_logical_number);
+		//pr_info("number of round : %d\n", block_logical_number);
 		sc = (sector_t)index->blocks[block_logical_number];
 		bh_data = sb_bread(inode->i_sb, sc);
 		/*If we are reading the last block of the file*/
@@ -64,21 +69,23 @@ ssize_t ouichefs_read(struct file *file, char __user *buff, size_t size,
 		else
 			available_size = OUICHEFS_BLOCK_SIZE - logical_pos;
 
-		pr_info("available size : %lu\n", available_size);
+		//pr_info("available size : %lu\n", available_size);
 		if (unlikely(available_size <= 0))
 			break;
 		/*Not read more than what we can read and what we want to read*/
 		to_read = min(available_size, remaining_read);
-		pr_info("to read : %d\n", to_read);
-		pr_info("logical pos : %d\n", logical_pos);
+		//pr_info("to read : %d\n", to_read);
+		//pr_info("logical pos : %d\n", logical_pos);
 		data = (char *)bh_data->b_data;
-		read = copy_to_user(buff + (total_read - 1),
+		read = copy_to_user(buff + (buff_offset - 1),
 				    (data + logical_pos), to_read);
+		//pr_info("copied to user : %d\n", read);
 		remaining_read -= to_read;
 		total_read += to_read;
 		block_logical_number++;
-		pr_info("block_logical_number : %d\n", block_logical_number);
-		pr_info("remaining read : %ld\n", remaining_read);
+		buff_offset += to_read;
+		//pr_info("block_logical_number : %d\n", block_logical_number);
+		//pr_info("remaining read : %ld\n", remaining_read);
 		/*
 		 * If we read a new block, for sure the cursor position in this block 
 		 * will be at the begining
