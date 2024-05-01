4c0aada34993c348da0660c656255401751f5d28
Harena Rakotondratsima <harenamisa02@gmail.com>
2024-05-01 14:21:02 +0200

    fix: to mount the disk

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,10 +13,10 @@ insmod /share/ouichefs.ko
 
 # mount a ouichefs test directory
 TESTDIR=~/tests-ouichefs
-if [ ! -d "$TESTDIR" ]; then
+# if [ ! -d "$TESTDIR" ]; then
   mkdir $TESTDIR
   mount /share/test.img $TESTDIR
-fi
+# fi
 
 # run the user executables
 cd $TESTDIR
