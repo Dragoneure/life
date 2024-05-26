2c84e4d36dc8347f066d639278b56cf557bbe4b9
Riwan <coefficriwan@gmail.com>
2024-05-26 13:59:15 +0200

    feat: test read with page cached limitation

--- src/user/test_cached.c
+++ src/user/test_cached.c
@@ -20,10 +20,25 @@ int test_read_cached()
 	lseek(fd, 0, SEEK_SET);
 	read(fd, rbuf, len);
 
+	ASSERT_EQ_BUF(rbuf, wbuf, len);
+
+	char wbuf2[] = "This is a different text\n";
+	size_t len2 = strlen(wbuf2) + 1;
+
+	lseek(fd, 0, SEEK_SET);
+	write(fd, wbuf2, len2);
+
+	// not updated because we did not change the write function
+	// so it's not detected as dirty, and still think that it's up-to-date
 	lseek(fd, 0, SEEK_SET);
 	read(fd, rbuf, len);
-
 	ASSERT_EQ_BUF(rbuf, wbuf, len);
+
+	// after cache flush, this should be updated
+	flush_cache();
+	lseek(fd, 0, SEEK_SET);
+	read(fd, rbuf, len);
+	ASSERT_EQ_BUF(rbuf, wbuf2, len);
 
 	return TEST_SUCCESS;
 }
